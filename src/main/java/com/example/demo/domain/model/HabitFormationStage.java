3620943c20265a43c19c333d6013081b831638c1
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-09-03 21:58:41 +0900

    remove : HabitFormationStage 양방향 관계 삭제

--- src/main/java/com/example/demo/domain/model/HabitFormationStage.java
+++ src/main/java/com/example/demo/domain/model/HabitFormationStage.java
@@ -8,7 +8,6 @@ public class HabitFormationStage {
 
     private Long id;
     private int stage;
-    private Habit habit;
     private List<String> questions;
     private List<String> answers;
     private String feedback;
