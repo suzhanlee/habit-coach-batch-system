9af8f366c7532c9ec50c1ea4f55fbfdda2a5c6fd
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-09-03 21:59:05 +0900

    remove : HabitTracking 양방향 관계 삭제

--- src/main/java/com/example/demo/domain/model/HabitTracking.java
+++ src/main/java/com/example/demo/domain/model/HabitTracking.java
@@ -1,18 +1,13 @@
 package com.example.demo.domain.model;
 
 import java.time.LocalDate;
+import lombok.Getter;
 
+@Getter
 public class HabitTracking {
 
     private Long id;
-    private Habit habit;
     private LocalDate completedDate;
-
-    public HabitTracking(Long id, Habit habit, LocalDate completedDate) {
-        this.id = id;
-        this.habit = habit;
-        this.completedDate = completedDate;
-    }
 
     public HabitTracking(Long id, LocalDate completedDate) {
         this.id = id;
